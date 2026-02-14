e9c7cce3b115b3c59e7aa3ac4df0cbc52ccdf731
Aleksandre Chokheli <a.chokheli@conceptdigital.com>
2026-02-14 18:51:44 +0400

    Add background color to main container in PageLayout component

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, ViewProps } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors } from '../../theme';
 
 interface PageLayoutProps extends ViewProps {
   children: React.ReactNode;
@@ -17,6 +18,7 @@ export const PageLayout: React.FC<PageLayoutProps> = ({ children, style, ...prop
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+    backgroundColor:colors.primary
   },
 });
 
